6bd9f5837309767088b99ea19d2218f13e3d62d9
Joe Marzullo <jmarzu13@gmail.com>
2020-08-31 10:27:19 -0700

    started tetris, WIP

--- src/app/tetris/tetris.component.ts
+++ src/app/tetris/tetris.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { COLS, BLOCK_SIZE, ROWS  } from '../../assets/constants';
 
 @Component({
   selector: 'app-tetris',
@@ -6,10 +7,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tetris.component.css']
 })
 export class TetrisComponent implements OnInit {
+  @ViewChild('board', { static: true })
+  canvas: ElementRef<HTMLCanvasElement>;
+
+  ctx: CanvasRenderingContext2D;
+  points: number;
+  lines: number;
+  level: number;
 
   constructor() { }
 
   ngOnInit() {
+    this.initBoard();
   }
+
+  initBoard() {
+    this.ctx = this.canvas.nativeElement.getContext('2d');
+
+    this.ctx.canvas.width = COLS * BLOCK_SIZE;
+    this.ctx.canvas.height = ROWS * BLOCK_SIZE;
+  }
+
+  getEmptyBoard(): number[][] {
+    return Array.from({ length: ROWS }, () => Array(COLS).fill(0));
+  }
+
+  // play() {
+  //   this.board = this.boardService.getEmptyBoard();
+  // }
 
 }
